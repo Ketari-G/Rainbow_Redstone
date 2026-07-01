75ba9d0903336abb768a114c32ea549f68630d26
Ketari <ketari.redstone@gmail.com>
2026-07-01 15:50:46 +0300

    v1.0.0 done (num colors + fixes)

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ This repo is mainly used for me to keep track of things and branches and file hi
 
 ### To be done:
 - v1.0.0
-    - numbers color customization
     - license file for resource pack itself
 - v1.1.0
     - add shapes (flat 16x16 square, 4x4 and 6x6 with shaded sides)
--- assets/minecraft/shaders/core/default_colors.glsl
+++ assets/minecraft/shaders/core/default_colors.glsl
@@ -4,67 +4,72 @@
 
 #ifndef respackopts_loaded
     //signal strengh 0
-    #define rainbowRedstone_color_ss0_r 0 //red
-    #define rainbowRedstone_color_ss0_g 0 //green
-    #define rainbowRedstone_color_ss0_b 0 //blue
+    #define rainbowRedstone_rcolor_ss0_r 0 //red
+    #define rainbowRedstone_rcolor_ss0_g 0 //green
+    #define rainbowRedstone_rcolor_ss0_b 0 //blue
     //signal strengh 1
-    #define rainbowRedstone_color_ss1_r 71 //red
-    #define rainbowRedstone_color_ss1_g 79 //green
-    #define rainbowRedstone_color_ss1_b 82 //blue
+    #define rainbowRedstone_rcolor_ss1_r 71 //red
+    #define rainbowRedstone_rcolor_ss1_g 79 //green
+    #define rainbowRedstone_rcolor_ss1_b 82 //blue
     //signal strengh 2
-    #define rainbowRedstone_color_ss2_r 156 //red
-    #define rainbowRedstone_color_ss2_g 157 //green
-    #define rainbowRedstone_color_ss2_b 151 //blue
+    #define rainbowRedstone_rcolor_ss2_r 156 //red
+    #define rainbowRedstone_rcolor_ss2_g 157 //green
+    #define rainbowRedstone_rcolor_ss2_b 151 //blue
     //signal strengh 3
-    #define rainbowRedstone_color_ss3_r 243 //red
-    #define rainbowRedstone_color_ss3_g 140 //green
-    #define rainbowRedstone_color_ss3_b 170 //blue
+    #define rainbowRedstone_rcolor_ss3_r 243 //red
+    #define rainbowRedstone_rcolor_ss3_g 140 //green
+    #define rainbowRedstone_rcolor_ss3_b 170 //blue
     //signal strengh 4
-    #define rainbowRedstone_color_ss4_r 198 //red
-    #define rainbowRedstone_color_ss4_g 79 //green
-    #define rainbowRedstone_color_ss4_b 189 //blue
+    #define rainbowRedstone_rcolor_ss4_r 198 //red
+    #define rainbowRedstone_rcolor_ss4_g 79 //green
+    #define rainbowRedstone_rcolor_ss4_b 189 //blue
     //signal strengh 5
-    #define rainbowRedstone_color_ss5_r 137 //red
-    #define rainbowRedstone_color_ss5_g 50 //green
-    #define rainbowRedstone_color_ss5_b 183 //blue
+    #define rainbowRedstone_rcolor_ss5_r 137 //red
+    #define rainbowRedstone_rcolor_ss5_g 50 //green
+    #define rainbowRedstone_rcolor_ss5_b 183 //blue
     //signal strengh 6
-    #define rainbowRedstone_color_ss6_r 60 //red
-    #define rainbowRedstone_color_ss6_g 68 //green
-    #define rainbowRedstone_color_ss6_b 169 //blue
+    #define rainbowRedstone_rcolor_ss6_r 60 //red
+    #define rainbowRedstone_rcolor_ss6_g 68 //green
+    #define rainbowRedstone_rcolor_ss6_b 169 //blue
     //signal strengh 7
-    #define rainbowRedstone_color_ss7_r 58 //red
-    #define rainbowRedstone_color_ss7_g 179 //green
-    #define rainbowRedstone_color_ss7_b 218 //blue
+    #define rainbowRedstone_rcolor_ss7_r 58 //red
+    #define rainbowRedstone_rcolor_ss7_g 179 //green
+    #define rainbowRedstone_rcolor_ss7_b 218 //blue
     //signal strengh 8
-    #define rainbowRedstone_color_ss8_r 22 //red
-    #define rainbowRedstone_color_ss8_g 156 //green
-    #define rainbowRedstone_color_ss8_b 157 //blue
+    #define rainbowRedstone_rcolor_ss8_r 22 //red
+    #define rainbowRedstone_rcolor_ss8_g 156 //green
+    #define rainbowRedstone_rcolor_ss8_b 157 //blue
     //signal strengh 9
-    #define rainbowRedstone_color_ss9_r 93 //red
-    #define rainbowRedstone_color_ss9_g 124 //green
-    #define rainbowRedstone_color_ss9_b 21 //blue
+    #define rainbowRedstone_rcolor_ss9_r 93 //red
+    #define rainbowRedstone_rcolor_ss9_g 124 //green
+    #define rainbowRedstone_rcolor_ss9_b 21 //blue
     //signal strengh 10
-    #define rainbowRedstone_color_ss10_r 128 //red
-    #define rainbowRedstone_color_ss10_g 199 //green
-    #define rainbowRedstone_color_ss10_b 31 //blue
+    #define rainbowRedstone_rcolor_ss10_r 128 //red
+    #define rainbowRedstone_rcolor_ss10_g 199 //green
+    #define rainbowRedstone_rcolor_ss10_b 31 //blue
     //signal strengh 11
-    #define rainbowRedstone_color_ss11_r 194 //red
-    #define rainbowRedstone_color_ss11_g 213 //green
-    #define rainbowRedstone_color_ss11_b 50 //blue
+    #define rainbowRedstone_rcolor_ss11_r 194 //red
+    #define rainbowRedstone_rcolor_ss11_g 213 //green
+    #define rainbowRedstone_rcolor_ss11_b 50 //blue
     //signal strengh 12
-    #define rainbowRedstone_color_ss12_r 255 //red
-    #define rainbowRedstone_color_ss12_g 216 //green
-    #define rainbowRedstone_color_ss12_b 61 //blue
+    #define rainbowRedstone_rcolor_ss12_r 255 //red
+    #define rainbowRedstone_rcolor_ss12_g 216 //green
+    #define rainbowRedstone_rcolor_ss12_b 61 //blue
     //signal strengh 13
-    #define rainbowRedstone_color_ss13_r 245 //red
-    #define rainbowRedstone_color_ss13_g 128 //green
-    #define rainbowRedstone_color_ss13_b 29 //blue
+    #define rainbowRedstone_rcolor_ss13_r 245 //red
+    #define rainbowRedstone_rcolor_ss13_g 128 //green
+    #define rainbowRedstone_rcolor_ss13_b 29 //blue
     //signal strengh 14
-    #define rainbowRedstone_color_ss14_r 176 //red
-    #define rainbowRedstone_color_ss14_g 46 //green
-    #define rainbowRedstone_color_ss14_b 38 //blue
+    #define rainbowRedstone_rcolor_ss14_r 176 //red
+    #define rainbowRedstone_rcolor_ss14_g 46 //green
+    #define rainbowRedstone_rcolor_ss14_b 38 //blue
     //signal strengh 15
-    #define rainbowRedstone_color_ss15_r 125 //red
-    #define rainbowRedstone_color_ss15_g 57 //green
-    #define rainbowRedstone_color_ss15_b 23 //blue
+    #define rainbowRedstone_rcolor_ss15_r 125 //red
+    #define rainbowRedstone_rcolor_ss15_g 57 //green
+    #define rainbowRedstone_rcolor_ss15_b 23 //blue
+
+    //numbers
+    #define rainbowRedstone_ncolor_r 255 //red
+    #define rainbowRedstone_ncolor_g 255 //green
+    #define rainbowRedstone_ncolor_b 255 //blue
 #endif
